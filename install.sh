6a20e6d11e6585ed077c521725d5324fda82e973
Zeebrow <mzborowski@yahoo.com>
2021-06-26 19:26:39 -0400

    I think the mess is over. Better install options

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-function install_scripts () {
-	cp colors $HOME/.local/bin/scripts/ && echo installing colors...
+function debug () {
+echo $0
+echo $BASH_SOURCE
+echo $(basename $0)
+pwd
+realpath $0
+realpath $BASH_SOURCE
+echo "-------------"
 }
 
-function cp_bashrc (){
+_bn="`pwd`/$(basename $0)"
+_rp="$(realpath $0)"
+[ "$_bn" != "$_rp" ] && echo "Must be run from same directory. Exiting!" &&  exit 1
+
+function install_scripts () {
+	source ./scripts/install-scripts
+}
+
+function install_bashrc (){
 	if test -f $HOME/.bashrc; then
 		cp $HOME/.bashrc $HOME/.bashrc.bkup.$(date +%s)
 	fi
 	echo installing bashrc...
 	cp ./.bashrc $HOME
 }
-function rm_backups (){
-	for file in `ls -1 $HOME/.bashrc.bkup.* 2> /dev/null`; do
-		echo "removing $file..."
-		rm $file
-	done
-}
 
-if [ $# -eq 0 ]; then
-	cp_bashrc
-	install_scripts
-else
-	while [ $# -gt 0 ]; do
-		case "$1" in
-			--clean) rm_backups;;
-			*) ;;
-		esac
-		shift
-	done
-fi
+install_scripts
+install_bashrc
+
