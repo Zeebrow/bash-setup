423611c302061adf9c8bfcb401098d0c4bcf23a3
Zeebrow <mzborowski@yahoo.com>
2021-10-11 19:26:13 -0400

    emo ready (I guess (._. ))

---
+++ completions/complete_emo.bash
@@ -0,0 +1,23 @@
+nocomplete=()
+_complete_emo () {
+	# $1 = name of command to be completed
+	# $2 = word being completed
+	# $3 = "word preceding the word being completed"
+	## so $3 = $1 when completing the command's first arg
+	
+	# Stop requesting completions if there is anything already completed
+	[ "${#COMP_WORDS[@]}" -gt 2 ] && return
+  local _emoji_names=(`emo -l | cut -d' ' -f1`)
+	COMPREPLY=($(compgen -W "${_emoji_names[*]}" "${COMP_WORDS[1]}"))
+	
+	_deboog(){
+		# on single <tab>
+		echo "$1 ------ $2 ---------- $3"
+		echo "${_emoji_names[*]}"
+		echo "NOCOMPLETE $nocomplete"
+	}
+	#_deboog
+}
+
+#complete -C _complete_vic vic
+complete -X "$nocomplete" -F _complete_emo emo
